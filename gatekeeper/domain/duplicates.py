fbf546e8373012f955f28c926d528c06926d31f5
Nel31 <ornel.whannou-de-dravo@epitech.eu>
2025-05-02 16:19:29 +0100

    Delete gatekeeper directory

--- gatekeeper/domain/duplicates.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-
-def detect_duplicates(df):
-    names = df['nom'] + ' ' + df['prenom']
-    c = Counter(names)
-    return [name for name, count in c.items() if count > 1]
