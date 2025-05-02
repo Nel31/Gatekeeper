69afba814d2ce5bb727d1b960272debdd6b0d281
Nel31 <ornel.whannou-de-dravo@epitech.eu>
2025-05-02 16:19:43 +0100

    Delete tests directory

--- tests/test_duplicates.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from gatekeeper.domain.duplicates import detect_duplicates
-
-def test_detect_duplicates():
-    df = pd.DataFrame({'nom': ['A','A'], 'prenom': ['X','X']})
-    dups = detect_duplicates(df)
-    assert dups == ['A X']
