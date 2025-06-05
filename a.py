9305724dd436a078277ace807408645b507724a4
Nel31 <ornel.whannou-de-dravo@epitech.eu>
2025-06-05 08:11:52 +0100

    Clean up: Remove Python cache files and virtual environment files

--- a.py
+++
@@ -1,6 +0,0 @@
-from colorama import Fore, Style, init
-init(autoreset=True)
-
-print(Fore.RED + Style.BRIGHT + "Ceci est un test en rouge et gras !" + Style.RESET_ALL)
-print(Fore.GREEN + "Ceci est en vert.")
-print("Ceci est normal.")
