9ca94638876015b5bd28fb28cd806c77394621ed
Nel31 <ornel.whannou-de-dravo@epitech.eu>
2025-06-16 09:52:44 +0100

    feat: add reset functionality with confirmation for back button

--- ui/widgets/__init__.py
+++ ui/widgets/__init__.py
@@ -4,6 +4,5 @@ Package widgets pour les composants UI réutilisables
 
 from .stat_widget import StatWidget
 from .file_drop_widget import FileDropWidget
-from .table_tooltip_helper import TableTooltipHelper
 
-__all__ = ['StatWidget', 'FileDropWidget', 'TableTooltipHelper']
+__all__ = ['StatWidget', 'FileDropWidget']
